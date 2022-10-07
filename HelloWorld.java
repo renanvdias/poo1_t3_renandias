e84870e318450b83d66238941d3de5d539189102
Renan Dias <renanvdias@github.com>
2022-10-06 21:26:04 -0300

    att

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {  
+   public static void main(String args[]) {  
+        System.out.println("Hello World");
+	System.out.println("Renan Vitor Amaral Dias");
+	System.out.println("12221BSI270");	     
+   }  
+}  

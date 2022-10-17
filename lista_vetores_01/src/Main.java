1eaf48048cb68175c682b6ebac37882aa824e0bd
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2022-10-17 20:39:35 -0300

    att

--- lista_vetores_01/src/Main.java
+++ lista_vetores_01/src/Main.java
@@ -2,15 +2,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int valores[] = new int [6];
+        int i = 0, resto;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Entre com os 6 números: ");
-        for (int i = 0; i < valores.length; i++)
-        {
+        do {
             valores[i] = entrada.nextInt();
-        }
+            resto = valores[i] % 10;
+            for (i = 1; i < valores.length; i++) {
+                valores[i] = entrada.nextInt();
+            }
+            valores[i] = valores[i] / 10;
+        }while (resto%2 != 0);
         System.out.println("Os valores lidos são :");
-        for (int i = 5; i >= 0; i--) {
-            System.out.println(valores[i]);
+        for (int j = 5; j >= 0; j--) {
+            System.out.println(valores[j]);
         }
     }
 }

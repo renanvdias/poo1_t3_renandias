40b8c2bca0127170121ca6b3d30001e77ebf299d
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2022-10-24 19:58:43 -0300

    att

--- LAB02 - First OOP code/ex01/Main.java
+++ LAB02 - First OOP code/ex01/Main.java
@@ -1,24 +1,32 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-
         Figurinha Jogador1 = new Figurinha();
         Figurinha Jogador2 = new Figurinha();
         Figurinha Jogador3 = new Figurinha();
         Jogador1.nome = "Pedro Correia";
         Jogador1.altura = 1.80;
-        Jogador1.data = "05/02/1990";
+        Jogador1.data = "20/05/1995";
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate localDate = LocalDate.parse(Jogador1.data, format);
         Jogador1.pais = "Brasil";
         Jogador1.peso = 80;
         Jogador1.posicao = "Atacante";
         Jogador2.nome = "Kloken Karkaus";
         Jogador2.altura = 1.78;
         Jogador2.data = "15/03/1995";
+        LocalDate.parse(Jogador2.data, format);
         Jogador2.pais = "Bélgica";
         Jogador2.peso = 85;
         Jogador2.posicao = "Goleiro";
         Jogador3.nome = "Bob Freakles";
         Jogador3.altura = 1.82;
         Jogador3.data = "10/11/1997";
+        LocalDate.parse(Jogador3.data, format);
         Jogador3.pais = "Estados Unidos";
         Jogador3.peso = 79;
         Jogador3.posicao = "Zagueiro";

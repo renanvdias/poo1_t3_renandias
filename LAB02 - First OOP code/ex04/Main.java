35e738ed1d6bdca5c12f5511faaa969ccad11b29
Renan Dias <renanvdias@github.com>
2022-11-06 18:00:47 -0300

    lab02 ex04

---
+++ LAB02 - First OOP code/ex04/Main.java
@@ -0,0 +1,38 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Selecao Sel_1 = new Selecao();
+        Sel_1.setNome_selecao("Brasil");
+        Sel_1.setJogador_titular_1("Bruno Costa");
+        Sel_1.setJogador_titular_2("Carlos Eduardo");
+        Sel_1.setJogador_titular_3("Gabriel Ferreira");
+        Sel_1.setJogador_titular_4("Lucas Ricardo");
+        Sel_1.setJogador_titular_5("Rodney dos Santos");
+        Sel_1.setJogador_titular_6("Fábio Jesus");
+        Sel_1.setJogador_titular_7("Fabricio Correia");
+        Sel_1.setJogador_titular_8("Arthur Fonseca");
+        Sel_1.setJogador_titular_9("Enzo da Cruz");
+        Sel_1.setJogador_titular_10("Gustavo Silva");
+        Sel_1.setJogador_titular_11("Rubens Alameda");
+        Sel_1.setTecnico("Cuca");
+        Selecao Sel_2 = new Selecao();
+        Sel_2.setNome_selecao("Colombia");
+        Sel_2.setJogador_titular_1("Ferrerito Colombo");
+        Sel_2.setJogador_titular_2("Walter Garciera");
+        Sel_2.setJogador_titular_3("Manoel Costa");
+        Sel_2.setJogador_titular_4("Murilo Morquenhe");
+        Sel_2.setJogador_titular_5("Sávio Silvestre");
+        Sel_2.setJogador_titular_6("Veltom Barroso");
+        Sel_2.setJogador_titular_7("Alexander Biltrawa");
+        Sel_2.setJogador_titular_8("Armando Marmero");
+        Sel_2.setJogador_titular_9("Pablo Arcero");
+        Sel_2.setJogador_titular_10("Pedro Santoz");
+        Sel_2.setJogador_titular_11("Felipe Castanhares");
+        Sel_2.setTecnico("Marco Santino");
+    }
+}
---
+++ LAB02 - First OOP code/ex04/Selecao.java
@@ -0,0 +1,119 @@
+public class Selecao {
+    private String nome_selecao;
+    private String jogador_titular_1;
+    private String jogador_titular_2;
+    private String jogador_titular_3;
+    private String jogador_titular_4;
+    private String jogador_titular_5;
+    private String jogador_titular_6;
+    private String jogador_titular_7;
+    private String jogador_titular_8;
+    private String jogador_titular_9;
+    private String jogador_titular_10;
+    private String jogador_titular_11;
+    private String tecnico;
+
+    public String getNome_selecao() {
+        return nome_selecao;
+    }
+
+    public void setNome_selecao(String nome_selecao) {
+        this.nome_selecao = nome_selecao;
+    }
+
+    public String getJogador_titular_1() {
+        return jogador_titular_1;
+    }
+
+    public void setJogador_titular_1(String jogador_titular_1) {
+        this.jogador_titular_1 = jogador_titular_1;
+    }
+
+    public String getJogador_titular_2() {
+        return jogador_titular_2;
+    }
+
+    public void setJogador_titular_2(String jogador_titular_2) {
+        this.jogador_titular_2 = jogador_titular_2;
+    }
+
+    public String getJogador_titular_3() {
+        return jogador_titular_3;
+    }
+
+    public void setJogador_titular_3(String jogador_titular_3) {
+        this.jogador_titular_3 = jogador_titular_3;
+    }
+
+    public String getJogador_titular_4() {
+        return jogador_titular_4;
+    }
+
+    public void setJogador_titular_4(String jogador_titular_4) {
+        this.jogador_titular_4 = jogador_titular_4;
+    }
+
+    public String getJogador_titular_5() {
+        return jogador_titular_5;
+    }
+
+    public void setJogador_titular_5(String jogador_titular_5) {
+        this.jogador_titular_5 = jogador_titular_5;
+    }
+
+    public String getJogador_titular_6() {
+        return jogador_titular_6;
+    }
+
+    public void setJogador_titular_6(String jogador_titular_6) {
+        this.jogador_titular_6 = jogador_titular_6;
+    }
+
+    public String getJogador_titular_7() {
+        return jogador_titular_7;
+    }
+
+    public void setJogador_titular_7(String jogador_titular_7) {
+        this.jogador_titular_7 = jogador_titular_7;
+    }
+
+    public String getJogador_titular_8() {
+        return jogador_titular_8;
+    }
+
+    public void setJogador_titular_8(String jogador_titular_8) {
+        this.jogador_titular_8 = jogador_titular_8;
+    }
+
+    public String getJogador_titular_9() {
+        return jogador_titular_9;
+    }
+
+    public void setJogador_titular_9(String jogador_titular_9) {
+        this.jogador_titular_9 = jogador_titular_9;
+    }
+
+    public String getJogador_titular_10() {
+        return jogador_titular_10;
+    }
+
+    public void setJogador_titular_10(String jogador_titular_10) {
+        this.jogador_titular_10 = jogador_titular_10;
+    }
+
+    public String getJogador_titular_11() {
+        return jogador_titular_11;
+    }
+
+    public void setJogador_titular_11(String jogador_titular_11) {
+        this.jogador_titular_11 = jogador_titular_11;
+    }
+
+    public String getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+}

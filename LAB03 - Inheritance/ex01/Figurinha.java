af3ec5219384b23b5ed228ffe1554001754fefcc
Renan Vitor Amaral Dias <12211bsi270@FACOM.LOCAL>
2022-11-21 19:54:37 -0300

    ex01_lab03

---
+++ LAB03 - Inheritance/ex01/Figurinha.java
@@ -0,0 +1,78 @@
+import java.sql.SQLOutput;
+
+public class Figurinha{
+        private String nome;
+        private String data;
+        private double altura;
+        private double peso;
+        private String posicao;
+        private String pais;
+
+        public Figurinha(String nome, String data, double altura, double peso, String posicao, String pais) {
+            this.nome = nome;
+            this.data = data;
+            this.altura = altura;
+            this.peso = peso;
+            this.posicao = posicao;
+            this.pais = pais;
+        }
+
+        public void MostrarFigurinha (){
+            System.out.println("---FIGURINHA---");
+            System.out.println("Nome: " + getNome());
+            System.out.println("Data de nascimento: " + getData());
+            System.out.println("Altura: " +  getAltura());
+            System.out.println("Peso: " + getPeso());
+            System.out.println("Posição: " + getPosicao());
+            System.out.println("País: " + getPais());
+            System.out.println("------------------");
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public double getAltura() {
+            return altura;
+        }
+
+        public void setAltura(double altura) {
+            this.altura = altura;
+        }
+
+        public double getPeso() {
+            return peso;
+        }
+
+        public void setPeso(double peso) {
+            this.peso = peso;
+        }
+
+        public String getPosicao() {
+            return posicao;
+        }
+
+        public void setPosicao(String posicao) {
+            this.posicao = posicao;
+        }
+
+        public String getPais() {
+            return pais;
+        }
+
+        public void setPais(String pais) {
+            this.pais = pais;
+        }
+    }

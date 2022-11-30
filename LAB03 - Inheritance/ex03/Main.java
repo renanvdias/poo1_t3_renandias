5301728082e03480764b64abd5a16a70ad183c77
Renan Dias <renanvdias@github.com>
2022-11-30 17:06:56 -0300

    ex03

--- LAB03 - Inheritance/ex03/Main.java
+++ LAB03 - Inheritance/ex03/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Figurinha f = new Figurinha("Carlos Mendes", "05/02/1995", 1.96, 89, "Atacante", "Brasil");
         f.MostrarFigurinha();
-        FigurinhaExtra fa = new FigurinhaExtra(f, "Legends", "Bronze");
-        fa.MostrarFigurinha();
+        FigurinhaExtra faa = new FigurinhaExtra(f, "Legends", "Bronze");
+        faa.MostrarFigurinha();
     }
 }

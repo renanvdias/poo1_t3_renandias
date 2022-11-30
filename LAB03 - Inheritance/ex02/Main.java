12ba95e8c6fdf22f6258794c0762dcf7d70d3844
Renan Dias <renanvdias@github.com>
2022-11-30 17:05:57 -0300

    ex03lab03

--- LAB03 - Inheritance/ex02/Main.java
+++ LAB03 - Inheritance/ex02/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
         Figurinha f = new Figurinha("Bruno Mendes", "05/02/1995", 1.96, 89, "Atacante", "Brasil");
         f.MostrarFigurinha();
         FigurinhaExtra fa = new FigurinhaExtra("Bart Kindle", "06/06/1999", 1.75, 78, "Goleiro", "Canada", "Legends", "Bronze");
